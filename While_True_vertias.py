68c076bdd1306f9e21db9bb439e7432d4a16b56b
PatricijaPav <95850561+PatricijaPav@users.noreply.github.com>
2022-04-19 21:40:28 +0300

    Add files via upload

---
+++ While_True_vertias.py
@@ -0,0 +1,10 @@
+while True:
+    darbiba = int(input("""Kādu darbību vēlaties veikt?
+    1- Izprintēt visu pieejamo produktu sarakstu
+    2- Izprintēt augļu pieejamo sarakstu
+    3- Izprintēt dārzeņu pieejamo sarakstu
+    4- Izprintēt piena produktu pieejamo sarakstu
+    5- Izprintēt gaļas produktu pieejamo sarakstu
+    6- Izprintēt graudaugu produktu pieejamo sarakstu
+    7- Izvēlēties produktus no pieejamajiem un veikt aprēķinu
+    8- Manuāli ievadīt produktus un saskaitīt to kaloriju daudzumu, tauku daudzumu un cukura daudzumu \n"""))
